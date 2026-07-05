7aedf96f234a891b1cd5fcf9965e977ef9d38744
Кирилл <mamasw11@gmail.com>
2026-07-05 13:42:16 +0300

    Update Bowl.java

--- Animal/Bowl.java
+++ Animal/Bowl.java
@@ -1,1 +1,39 @@
+public class Bowl {
+    private int foodAmount;
 
+    public Bowl(int foodAmount) {
+        this.foodAmount = Math.max(foodAmount, 0); 
+    }
+
+    // Метод для добавления еды в миску
+    public void addFood(int amount) {
+        if (amount <= 0) {
+            System.out.println("Нельзя добавить отрицательное или нулевое количество еды!");
+            return;
+        }
+        foodAmount += amount;
+        System.out.println("В миску добавлено " + amount + " еды. Теперь в миске " + foodAmount + " еды.");
+    }
+
+    // Метод для уменьшения еды 
+    public void decreaseFood(int amount) {
+        if (amount <= 0) {
+            return;
+        }
+        foodAmount -= amount;
+        // еда не станет отрицательной
+        if (foodAmount < 0) {
+            foodAmount = 0;
+        }
+    }
+
+    // Геттер для количества еды
+    public int getFood() {
+        return foodAmount;
+    }
+
+    // Метод для вывода информации о миске
+    public void printInfo() {
+        System.out.println("В миске " + foodAmount + " еды.");
+    }
+}
