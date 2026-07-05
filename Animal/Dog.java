07081d8b40e41b8a392e441dfbac0c8dfa812991
Кирилл <mamasw11@gmail.com>
2026-07-05 13:41:19 +0300

    Update Dog.java

--- Animal/Dog.java
+++ Animal/Dog.java
@@ -1,1 +1,18 @@
 
+public class Dog extends Animal {
+    private static int dogCount = 0;
+
+    // Константы для ограничений
+    private static final int DOG_RUN_LIMIT = 500;
+    private static final int DOG_SWIM_LIMIT = 10;
+
+    public Dog(String name) {
+        super(name, DOG_RUN_LIMIT, DOG_SWIM_LIMIT);
+        dogCount++;
+    }
+
+    // Статический метод для получения количества собак
+    public static int getDogCount() {
+        return dogCount;
+    }
+}
