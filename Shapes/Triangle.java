c1061e41ca9f61def4033a0c1a102eed696a2041
Кирилл <mamasw11@gmail.com>
2026-07-05 14:20:43 +0300

    Update Triangle.java

--- Shapes/Triangle.java
+++ Shapes/Triangle.java
@@ -1,1 +1,64 @@
-
+public class Triangle implements Shape {
+    private double sideA;
+    private double sideB;
+    private double sideC;
+    private String fillColor;
+    private String borderColor;
+    
+    public Triangle(double sideA, double sideB, double sideC, String fillColor, String borderColor) {
+        // Проверка на существование треугольника
+        if (sideA + sideB <= sideC || sideA + sideC <= sideB || sideB + sideC <= sideA) {
+            throw new IllegalArgumentException("Треугольник с такими сторонами не существует!");
+        }
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+        this.fillColor = fillColor;
+        this.borderColor = borderColor;
+    }
+    
+    @Override
+    public double calculatePerimeter() {
+        return sideA + sideB + sideC;
+    }
+    
+    @Override
+    public double calculateArea() {
+        // Формула Герона
+        double p = calculatePerimeter() / 2;
+        return Math.sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
+    }
+    
+    @Override
+    public String getFillColor() {
+        return fillColor;
+    }
+    
+    @Override
+    public String getBorderColor() {
+        return borderColor;
+    }
+    
+    @Override
+    public void setFillColor(String color) {
+        this.fillColor = color;
+    }
+    
+    @Override
+    public void setBorderColor(String color) {
+        this.borderColor = color;
+    }
+    
+    // Геттеры
+    public double getSideA() {
+        return sideA;
+    }
+    
+    public double getSideB() {
+        return sideB;
+    }
+    
+    public double getSideC() {
+        return sideC;
+    }
+}
