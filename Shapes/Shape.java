f97caaa06000073c899aae37e0854e8fa0238362
Кирилл <mamasw11@gmail.com>
2026-07-05 14:18:58 +0300

    Update Shape.java

--- Shapes/Shape.java
+++ Shapes/Shape.java
@@ -1,1 +1,22 @@
-
+public interface Shape extends Colorable {
+    
+    // Дефолтный метод для расчета периметра 
+    default double calculatePerimeter() {
+        return 0;
+    }
+    
+    // Дефолтный метод для расчета площади 
+    default double calculateArea() {
+        return 0;
+    }
+    
+    // Метод для вывода информации о фигуре
+    default void printInfo() {
+        System.out.println("--- " + getClass().getSimpleName() + " ---");
+        System.out.println("Периметр: " + calculatePerimeter());
+        System.out.println("Площадь: " + calculateArea());
+        System.out.println("Цвет заливки: " + getFillColor());
+        System.out.println("Цвет границы: " + getBorderColor());
+        System.out.println();
+    }
+}
